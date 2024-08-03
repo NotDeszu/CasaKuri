74ebf6d66e974333bbbdd377697e3f2155fbb4a0
Felipe <cgogearsh4zb@gmail.com>
2024-08-02 20:52:32 -0600

    index animation

--- Front/ogani/index.php
+++ Front/ogani/index.php
@@ -28,6 +28,7 @@ include "../../funciones/usuario.php";
     <link rel="stylesheet" href="css/owl.carousel.min.css" type="text/css">
     <link rel="stylesheet" href="css/slicknav.min.css" type="text/css">
     <link rel="stylesheet" href="css/style.css" type="text/css">
+    <link rel="stylesheet" href="css/cosas.css" type="text/css">
 </head>
 
 <body>
@@ -142,22 +143,11 @@ include "../../funciones/usuario.php";
                     <div class="hero__search">
                         <div class="hero__search__form">
                             <form action="#">
-                                <!-- <div class="hero__search__categories">
-                                    categorias
-                                    <span class="arrow_carrot-down"></span>
-                                </div> -->
-                                <input type="text" placeholder="What do yo u need?">
-                                <button type="submit" class="site-btn">Buscar</button>
+                                <a class="btn-shine">Casa kuri Donde cada idea encuentra su herramienta perfecta</a>
                             </form>
                         </div>
                         <div class="hero__search__phone">
-                            <div class="hero__search__phone__icon">
-                                <i class="fa fa-phone"></i>
-                            </div>
-                            <div class="hero__search__phone__text">
-                                <h5>+65 11.188.888</h5>
-                                <span>support 24/7 time</span>
-                            </div>
+                            <button class="btn">Casa kuri</button>
                         </div>
                     </div>
                     <!--EMPIEZA seccion de banner?-->
